9e71f5c06ab4b68a3582e79b1d6c4c0515b0864e
avecging <avecgin@gmail.com>
2026-09-19 19:20:13 +0800

    Fail closed on unsafe stamp credit links

--- src/components/stamps/StampCredit.tsx
+++ src/components/stamps/StampCredit.tsx
@@ -3,6 +3,7 @@ import type { ShopStampDesign } from '@/src/domain/shop-detail';
 export function StampCredit({stamp}:{stamp:ShopStampDesign}) {
   const name=stamp.uploaded?.creatorName ?? stamp.commissioned?.illustratorCredit;
   const url=stamp.uploaded?.creatorUrl ?? stamp.commissioned?.illustratorCreditUrl;
+  const safeUrl=typeof url==='string' && /^https?:\/\//i.test(url) ? url : undefined;
   if(!name) return null;
-  return <p>created by: {url ? <a href={url} target="_blank" rel="noreferrer">{name}</a> : name}</p>;
+  return <p>created by: {safeUrl ? <a href={safeUrl} target="_blank" rel="noreferrer">{name}</a> : name}</p>;
 }
